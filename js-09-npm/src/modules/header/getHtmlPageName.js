f7d5d074b462063b8c34d2a4906514f31b010f92
DavidAvellaneda97 <git@github.com:DavidAvellaneda97/Entregables-CH50.git>
2025-02-13 10:17:15 -0600

    js-09

--- js-09-npm/src/modules/header/getHtmlPageName.js
+++ js-09-npm/src/modules/header/getHtmlPageName.js
@@ -1,7 +1,10 @@
 
-
+/**
+ * obtener el nombre de la pagina HTML
+ * @param {string} filePath url del archivo html
+ * @returns el nombre de la pagina html
+ */                        
 const getHtmlPageName = ( filePath ) => {
     return filePath.split("/").pop();
-}
-
+}                       
 export { getHtmlPageName };
